a0471180c0086e5ed8f5f6c7af7582f3e99f0147
yccheok <yancheng.cheok@gmail.com>
2021-02-27 23:15:40 +0800

    Initial creation to be able to load plain_note from json files.

---
+++ UICollectionView-01/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  UICollectionView-01
+//
+//  Created by Cheok Yan Cheng on 27/02/2021.
+//
+
+import Foundation
---
+++ UICollectionView-01/log/Log.swift
@@ -0,0 +1,8 @@
+//
+//  Log.swift
+//  UICollectionView-01
+//
+//  Created by Cheok Yan Cheng on 27/02/2021.
+//
+
+import Foundation
