af60ff2fa3a0abf375d2b86e004469f8b4db414a
yccheok <yancheng.cheok@gmail.com>
2021-03-04 22:42:22 +0800

    Not sure why compact list for body only is broken...

--- UICollectionView-01/view/NoteCell.swift
+++ UICollectionView-01/view/NoteCell.swift
@@ -14,6 +14,7 @@ class NoteCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bodyLabel: UILabel!
     @IBOutlet var labelLabel: UILabel!
+    @IBOutlet var reminderLabel: UILabel!
     
     @IBOutlet var bodyLabelAndBottomStackViewConstraint: NSLayoutConstraint!
     @IBOutlet var bodyLabelAndBottomStackViewGreaterThanConstraint: NSLayoutConstraint!
@@ -21,22 +22,22 @@ class NoteCell: UICollectionViewCell {
     @IBOutlet var bodyLabelZeroHeightConstraint: NSLayoutConstraint!
     @IBOutlet var titleLabelAndBodyLabelConstraint: NSLayoutConstraint!
     @IBOutlet var labelLabelZeroHeightConstraint: NSLayoutConstraint!
+    @IBOutlet var reminderLabelZeroHeightConstraint: NSLayoutConstraint!
+    @IBOutlet var bottomStackViewZeroHeightConstraint: NSLayoutConstraint!
     
     var layout: Layout?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        
-        titleLabelZeroHeightConstraint.isActive = false
-        bodyLabelZeroHeightConstraint.isActive = false
     }
 
     func setup(_ plainNote: PlainNote) {
         titleLabel.text = plainNote.title
         bodyLabel.text = plainNote.body
         
-        labelLabel.text = nil
+        labelLabel.text = "Work"
+        reminderLabel.text = nil//"Sat"
     }
     
     func updateLayout(_ layout: Layout) {
@@ -62,9 +63,12 @@ class NoteCell: UICollectionViewCell {
         let isTitleLabelEmpty = String.isNullOrEmpty(titleLabel.text)
         let isBodyLabelEmpty = String.isNullOrEmpty(bodyLabel.text)
         let isLabelLabelEmpty = String.isNullOrEmpty(labelLabel.text)
+        let isReminderLabelEmpty = String.isNullOrEmpty(reminderLabel.text)
+        
         var titleLabelIsHidden = false
         var bodyLabelIsHidden = false
         var labelLabelIsHidden = false
+        var reminderLabelIsHidden = false
         
         bodyLabelAndBottomStackViewConstraint.isActive = false
         bodyLabelAndBottomStackViewGreaterThanConstraint.isActive = true
@@ -94,18 +98,28 @@ class NoteCell: UICollectionViewCell {
             labelLabelZeroHeightConstraint.isActive = false
         }
         
+        if isReminderLabelEmpty {
+            reminderLabelIsHidden = true
+            reminderLabelZeroHeightConstraint.isActive = true
+        } else {
+            reminderLabelIsHidden = false
+            reminderLabelZeroHeightConstraint.isActive = false
+        }
+        
         if titleLabelIsHidden || bodyLabelIsHidden {
             titleLabelAndBodyLabelConstraint.constant = 0
         } else {
             titleLabelAndBodyLabelConstraint.constant = NoteCell.padding
         }
         
-        if labelLabelIsHidden {
+        if labelLabelIsHidden && reminderLabelIsHidden {
             bodyLabelAndBottomStackViewConstraint.constant = 0
             bodyLabelAndBottomStackViewGreaterThanConstraint.constant = 0
+            bottomStackViewZeroHeightConstraint.isActive = true
         } else {
             bodyLabelAndBottomStackViewConstraint.constant = NoteCell.padding
             bodyLabelAndBottomStackViewGreaterThanConstraint.constant = NoteCell.padding
+            bottomStackViewZeroHeightConstraint.isActive = false
         }
     }
     
@@ -117,9 +131,12 @@ class NoteCell: UICollectionViewCell {
         let isTitleLabelEmpty = String.isNullOrEmpty(titleLabel.text)
         let isBodyLabelEmpty = String.isNullOrEmpty(bodyLabel.text)
         let isLabelLabelEmpty = String.isNullOrEmpty(labelLabel.text)
+        let isReminderLabelEmpty = String.isNullOrEmpty(reminderLabel.text)
+        
         var titleLabelIsHidden = false
         var bodyLabelIsHidden = false
         var labelLabelIsHidden = false
+        var reminderLabelIsHidden = false
         
         bodyLabelAndBottomStackViewConstraint.isActive = true
         bodyLabelAndBottomStackViewGreaterThanConstraint.isActive = false
@@ -149,18 +166,28 @@ class NoteCell: UICollectionViewCell {
             labelLabelZeroHeightConstraint.isActive = false
         }
         
+        if isReminderLabelEmpty {
+            reminderLabelIsHidden = true
+            reminderLabelZeroHeightConstraint.isActive = true
+        } else {
+            reminderLabelIsHidden = false
+            reminderLabelZeroHeightConstraint.isActive = false
+        }
+        
         if titleLabelIsHidden || bodyLabelIsHidden {
             titleLabelAndBodyLabelConstraint.constant = 0
         } else {
             titleLabelAndBodyLabelConstraint.constant = NoteCell.padding
         }
         
-        if labelLabelIsHidden {
+        if labelLabelIsHidden && reminderLabelIsHidden {
             bodyLabelAndBottomStackViewConstraint.constant = 0
             bodyLabelAndBottomStackViewGreaterThanConstraint.constant = 0
+            bottomStackViewZeroHeightConstraint.isActive = true
         } else {
             bodyLabelAndBottomStackViewConstraint.constant = NoteCell.padding
             bodyLabelAndBottomStackViewGreaterThanConstraint.constant = NoteCell.padding
+            bottomStackViewZeroHeightConstraint.isActive = false
         }
     }
     
@@ -168,9 +195,12 @@ class NoteCell: UICollectionViewCell {
         let isTitleLabelEmpty = String.isNullOrEmpty(titleLabel.text)
         let isBodyLabelEmpty = String.isNullOrEmpty(bodyLabel.text)
         let isLabelLabelEmpty = String.isNullOrEmpty(labelLabel.text)
+        let isReminderLabelEmpty = String.isNullOrEmpty(reminderLabel.text)
+        
         var titleLabelIsHidden = false
         var bodyLabelIsHidden = false
         var labelLabelIsHidden = false
+        var reminderLabelIsHidden = false
         
         bodyLabelAndBottomStackViewConstraint.isActive = true
         bodyLabelAndBottomStackViewGreaterThanConstraint.isActive = false
@@ -201,18 +231,28 @@ class NoteCell: UICollectionViewCell {
             labelLabelZeroHeightConstraint.isActive = false
         }
         
+        if isReminderLabelEmpty {
+            reminderLabelIsHidden = true
+            reminderLabelZeroHeightConstraint.isActive = true
+        } else {
+            reminderLabelIsHidden = false
+            reminderLabelZeroHeightConstraint.isActive = false
+        }
+        
         if titleLabelIsHidden || bodyLabelIsHidden {
             titleLabelAndBodyLabelConstraint.constant = 0
         } else {
             titleLabelAndBodyLabelConstraint.constant = NoteCell.padding
         }
         
-        if labelLabelIsHidden {
+        if labelLabelIsHidden && reminderLabelIsHidden {
             bodyLabelAndBottomStackViewConstraint.constant = 0
             bodyLabelAndBottomStackViewGreaterThanConstraint.constant = 0
+            bottomStackViewZeroHeightConstraint.isActive = true
         } else {
             bodyLabelAndBottomStackViewConstraint.constant = NoteCell.padding
             bodyLabelAndBottomStackViewGreaterThanConstraint.constant = NoteCell.padding
+            bottomStackViewZeroHeightConstraint.isActive = false
         }
     }
 }
